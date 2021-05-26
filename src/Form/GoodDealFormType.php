7a43d695cbe583c06e18e142293399d3a46cb061
brand <maxime.brandon@etu.uca.fr>
2021-05-26 09:15:04 +0200

    Modification page creation of good deal #4

--- src/Form/GoodDealFormType.php
+++ src/Form/GoodDealFormType.php
@@ -4,12 +4,9 @@ namespace App\Form;
 
 use App\Entity\GoodDeal;
 use App\Entity\Group;
-use Doctrine\DBAL\Types\ArrayType;
-use Doctrine\DBAL\Types\BooleanType;
-use Doctrine\DBAL\Types\DateType;
-use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -27,30 +24,46 @@ class GoodDealFormType extends AbstractType
         $builder
             ->add('title', TextType::class,[
                 'required' => true,
+                'label' => 'Titre :',
+                'attr' => array('class'=> 'form-control mb-2')
             ])
             ->add('description', TextType::class,[
                 'required' => true,
+                'label' => 'Description :',
+                'attr' => array('class'=> 'form-control mb-2')
             ])
             ->add('link', TextType::class,[
                 'required' => true,
+                'label' => 'Lien du deal :',
+                'attr' => array('class'=> 'form-control mb-2')
             ])
             ->add('actualPrice', IntegerType::class,[
                 'required' => true,
+                'label' => 'Prix actuel :',
+                'attr' => array('class'=> 'form-control mb-2')
             ])
             ->add('newPrice',IntegerType::class,[
                 'required' => true,
+                'label' => 'Nouveau prix :',
+                'attr' => array('class'=> 'form-control mb-2')
             ])
-            ->add('freeDelivery',BooleanType::class,[
-                'required' => true,
+            ->add('freeDelivery',CheckboxType::class,[
+                'required' => false,
+                'label' => 'Livraison gratuite :  ',
+                'attr' => array('class'=> 'form-check-input mb-2')
             ])
-            ->add('groupsList',ArrayType::class,[
+            ->add('groupsList',EntityType::class,[
                 'class' => Group::class,
-                'choice_label' => 'Groupes :',
+                'label' => 'Groupes :',
                 'expanded' => false,
                 'multiple' => true,
                 'required' => false,
+                'attr' => array('class'=> 'form-select mb-2')
             ])
-            ->add('save', SubmitType::class, ['label' => 'Créer le deal']);
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer le deal',
+                'attr' => array('class'=> 'btn btn-primary')
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
