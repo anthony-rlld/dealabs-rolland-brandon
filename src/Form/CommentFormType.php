55eaaa6e8466baa9bb03a8420aa7639a91f623e1
Anthony Rolland <anthony.rlld@gmail.com>
2021-05-31 09:54:44 +0200

    Add comment management

---
+++ src/Form/CommentFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class,[
+                'required' => true,
+                'label' => 'Titre :',
+                'attr' => array('class'=> 'form-control mb-2')
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'label' => 'Description :',
+                'attr' => array('class' => 'form-control mb-2')
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => array('class'=> 'btn btn-primary')
+            ]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
